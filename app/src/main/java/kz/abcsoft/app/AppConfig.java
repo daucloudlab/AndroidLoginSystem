b40b2874fc09dab24021378c01ca19c3c184cdb9
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-25 23:49:13 +0600

    Android Login and registration system with volley library

---
+++ app/src/main/java/kz/abcsoft/app/AppConfig.java
@@ -0,0 +1,9 @@
+package kz.abcsoft.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.56.1/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.56.1/android_login_api/";
+}
